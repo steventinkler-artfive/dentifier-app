f0f6b87b902c4389283b3ea7d77c5f68c214eb98
Steven Tinkler <steven.tinkler@googlemail.com>
2025-12-07 12:41:58 +0000

    File changes

--- src/components/assessment/PhotoCapture.jsx
+++ src/components/assessment/PhotoCapture.jsx
@@ -577,7 +577,7 @@ export default function PhotoCapture({ initialPhotos = [], initialDamageItems = 
                 className="w-full bg-slate-800 border-slate-700 text-white hover:bg-slate-700 font-semibold"
               >
                 <Plus className="w-4 h-4 mr-2" />
-                Add More Damage
+                Add more Damage
               </Button>
             )}
           </div>
@@ -654,7 +654,7 @@ export default function PhotoCapture({ initialPhotos = [], initialDamageItems = 
                 className="w-full bg-slate-800 border-slate-700 text-white hover:bg-slate-700 font-semibold"
               >
                 <Plus className="w-4 h-4 mr-2" />
-                Add More Panel
+                Add more Panel
               </Button>
             )}
           </div>
