c89193f78b46711e835a7c00454087d61ceeb13f
Steven Tinkler <steven.tinkler@googlemail.com>
2026-04-25 12:14:41 +0000

    File changes

--- base44/functions/dvlaLookup/entry.ts
+++ base44/functions/dvlaLookup/entry.ts
@@ -1,6 +1,7 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.25';
 
 Deno.serve(async (req) => {
+  console.log('[DVLA] Handler reached');
   try {
     const base44 = createClientFromRequest(req);
     const user = await base44.auth.me();
